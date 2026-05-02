ce8f3674c18c8cc24081f0781b3ec1dac49db7ae
Raja Wardet Wahaj <wardetwahaj473@gmail.com>
2026-05-02 17:35:29 +0500

    Update api.py

--- frontend/backend/api.py
+++ frontend/backend/api.py
@@ -1109,4 +1109,6 @@ def password_reset_confirm(request: auth.PasswordResetConfirm) -> dict:
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port, log_level="info")
+    
