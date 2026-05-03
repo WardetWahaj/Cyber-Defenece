806bfb570edaaa1e001ad6d8d32f6778fa399c27
Raja Wardet Wahaj <wardetwahaj473@gmail.com>
2026-05-03 16:50:06 +0500

    Add install script for Nuclei v3.7.1
    Add bin/install_nuclei.sh which downloads the Nuclei v3.7.1 release zip, unzips it, moves the nuclei binary to /app/bin, sets executable permissions, and removes the zip archive. Provides a simple scripted installation step for CI/container images.

---
+++ bin/install_nuclei.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Installing Nuclei..."
+NUCLEI_VERSION="3.7.1"
+wget -q https://github.com/projectdiscovery/nuclei/releases/download/v${NUCLEI_VERSION}/nuclei_${NUCLEI_VERSION}_linux_amd64.zip
+unzip -q nuclei_${NUCLEI_VERSION}_linux_amd64.zip
+mv nuclei /app/bin/nuclei
+chmod +x /app/bin/nuclei
+rm nuclei_${NUCLEI_VERSION}_linux_amd64.zip
+echo "Nuclei installed successfully!"
